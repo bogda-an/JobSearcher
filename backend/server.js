5bd82ac0e817411595996b069c0298bd02564737
Bogdan <bogdanworks0@gmail.com>
2024-06-08 00:54:02 +0100

    version 4 / server work without problems

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
+require('dotenv').config();
 const express = require('express');
-const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
+
 const jobRoutes = require('./routes/jobRoutes');
-const authMiddleware = require('./middleware/authMiddleware');
-
-dotenv.config();
-
-connectDB();
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
+
+// Connect to database
+connectDB();
+
+// Middleware
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
+// Routes
 app.use('/api/jobs', jobRoutes);
+app.use('/api/users', userRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -26,6 +28,6 @@ app.use((req, res, next) => {
   res.status(404).send({ message: 'Route not found' });
 });
 
+// Start server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
